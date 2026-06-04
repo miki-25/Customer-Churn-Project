bf9e2a349b84be11b72c61846c9c35f219585649
miki-25 <mkd10a14@gmail.com>
2026-06-04 21:24:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import streamlit as st
+installed = [m.name for m in pkgutil.iter_modules()]
+
+st.write("JOBLIB INSTALLED:", "joblib" in installed)
+st.write("LIGHTGBM INSTALLED:", "lightgbm" in installed)
+
 import joblib
 import pandas as pd
 import lightgbm as lgb
 import plotly.graph_objects as go
+import pkgutil
+
+
 
 # ---------------------------------------------------
 # PAGE CONFIG
